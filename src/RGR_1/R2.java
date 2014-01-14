dbce331d62482e6b509c6ba5fa269a2c643417ae
Isaeff Sergiy <isaeffs06@gmail.com>
2014-01-14 18:24:59 +0200

    Исправления

--- src/RGR_1/R2.java
+++ src/RGR_1/R2.java
@@ -1,3 +1,5 @@
+
+
 package RGR_1;
 import java.util.*;
 public class R2 {
--- src/RGR_2/Main.java
+++ src/RGR_2/Main.java
@@ -1,19 +1,35 @@
 package RGR_2;
 
 public class Main {
-	//оПХЛЕП МЮУНФДЕМХЕ яХМСЯЮ
-	public static double getSumSinuses(double[] angles){
+	
+	//ме люяяхб ю я йюфдшл вкемнл нрдекэмн, ясллхпсел б лехм!
+	
+	//он сякнбхч днкфмн ашрэ public static double аег []!
+	public static double[] getSumSinuses(double[] angles, int type){
+		double [] LAngles = new double [2];	//LAngles = LocalAngles	
 		
 		for (int i=0; i < angles.length;i++){
-			int x = (int) Math.cos(angles[i]);
+			if (type == 0){
+			LAngles[i] = Math.cos(Math.toDegrees(angles[i]));
+		}	
+			else {
+					if (type == 1){
+						LAngles[i] = Math.sin(Math.toDegrees(angles[i]));
+					}
+			}
 		}
-		return 0;
+		return LAngles;
+		
 	}
 	
 	
 
 	public static void main(String[] args) {
 		double[]angles = new double[2];
-		System.out.println(Math.cos(angles[1]));
+		angles [0] = 50;
+		angles [1] = 35;
+  		
+		getSumSinuses(angles, 0);
+		System.out.println (angles[1]);
 	}
 }
